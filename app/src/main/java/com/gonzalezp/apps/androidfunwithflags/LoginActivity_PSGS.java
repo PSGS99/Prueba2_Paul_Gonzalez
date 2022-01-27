d4a2c8348fb1ea00ee8641465d95b44bb0e36fe4
PSGS99 <pgonzalez4756@uta.edu.ec>
2022-01-26 19:46:47 -0500

    Gonzalez Paul 19:45

--- app/src/main/java/com/gonzalezp/apps/androidfunwithflags/LoginActivity_PSGS.java
+++ app/src/main/java/com/gonzalezp/apps/androidfunwithflags/LoginActivity_PSGS.java
@@ -1,11 +1,14 @@
 package com.gonzalezp.apps.androidfunwithflags;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
 public class LoginActivity_PSGS extends AppCompatActivity {
 
@@ -25,11 +28,30 @@ public class LoginActivity_PSGS extends AppCompatActivity {
 
         String usr = user.getText().toString();
         String pas = pass.getText().toString();
+        StringBuilder text = new StringBuilder();
+        File file1 = new File(usr1);
+        File file2 = new File(usr2);
 
-        if ((usr == "usr1" && pas == "usr1") || (usr == "usr2" && pas == "usr2") || (!usr.isEmpty() && !pas.isEmpty())){
+        if ((usr == "usr1" && pas == "usr1") || (usr == "usr2" && pas == "usr2") || (!usr.isEmpty() && !pas.isEmpty())) {
+
+            if (usr == "usr1"){
+                try{
+                    BufferedReader read = new BufferedReader(new FileReader(file1));
+
+                }catch (Exception ex){
+                    ex.getMessage();
+                }
+            }else{
+
+            }
+
+
+
             Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("key_user", usr);
             startActivity(intent);
         }
+    }
 
         /*if (!nom.isEmpty() && !ape.isEmpty()) {
             Intent intent = new Intent(this, MainActivity.class);
@@ -39,7 +61,5 @@ public class LoginActivity_PSGS extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Nombre y Apellido requeridos", Toast.LENGTH_LONG).show();
         }*/
-
-    }
 
 }
